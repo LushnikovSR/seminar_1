45f074ccce0d394ac6034611c01bde158dfbb6a5
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-27 19:59:47 +0300

    add Task 003

---
+++ Task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача № 5
+// Напишите программу, которая на вход принимает одно положительное число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int revNumber = -1 * number;
+while(revNumber <= number)
+{
+    Console.Write($"{revNumber} ");
+    revNumber++;
+}
